0d38657110956b6c89dd2e4ea546eb7cd71327c3
ticcenter <halcon_luis15@hotmail.com>
2023-04-24 16:37:31 -0500

    TERCER commit

--- src/index.js
+++ src/index.js
@@ -1,93 +1,103 @@
-const express = require('express');
-const { COINBASE_API_KEY,
-    COINBASE_WEBHOOK_SECRET,
-    DOMAIN } = require('./config');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
 
-const { Client, resources, Webhook } = require('coinbase-commerce-node');    
-
-const morgan = require('morgan');
-
-Client.init(COINBASE_API_KEY);
+const { Client, resources, Webhook } = require("coinbase-commerce-node");
+const {
+  COINBASE_API_KEY,
+  COINBASE_WEBHOOK_SECRET,
+  DOMAIN,
+} = require("./config");
 
 const { Charge } = resources;
+Client.init(COINBASE_API_KEY);
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(cors());
+app.use(morgan("dev"));
 
+// add verify in order to process rawBody
 app.use(
-    express.json({
-        verify: (req, res, buf) => {
-            req.rawBody = buf
-        }
-    })
-)
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf;
+    },
+  })
+);
 
-app.get('/create-charge', async (rq, res) => {
+app.get("/create-charge", async (req, res) => {
+  const chargeData = {
+    name: "Sound Effect",
+    description: "An awesome science fiction sound effect",
+    local_price: {
+      amount: 0.2,
+      currency: "USD",
+    },
+    pricing_type: "fixed_price",
+    metadata: {
+      customer_id: "id_1005",
+      customer_name: "Satoshi Nakamoto",
+    },
+    redirect_url: `${DOMAIN}/success-payment`,
+    cancel_url: `${DOMAIN}/cancel-payment`,
+  };
 
-    const chargeData = {
-        name: 'Futuro',
-        description: 'Inversion',
-        local_price: {
-            amount: '0.2',
-            currency: 'USD'
-        },
-        pricing_type: 'fixed_price',
-        //Mostrar los datos del usuario
-        metadata: {
-            customer_id: 'id_123',
-            customer_name: 'Satoshi Nakamoto'
-        },
-        redirect_url: `${DOMAIN}/success-payment`,
-        cancel_url: `${DOMAIN}/cancel-payment`,
-    
-    }
-    const charge = await Charge.create(chargeData);
+  const charge = await Charge.create(chargeData);
 
-    res.send(charge);
-})
+  console.log(charge);
 
-app.post('/payment-handler', (req, res) => {
-    const rawBody = req.rawBody;
-    const signature = req.headers['x-cc-webhook-signature'];
-    const webhookSecret = COINBASE_WEBHOOK_SECRET;
-
-    let event;
-
-    try {
-        event = Webhook.verifyEventBody(rawBody, signature, webhookSecret)
-
-        if(event.type === 'charge:pending') {
-            console.log('Charge is pending')
-        }
-        if(event.type === 'charge:confirmed') {
-            console.log('Charge is confirmed')
-        }
-        if(event.type === 'charge:failed') {
-            console.log('Charge is failed')
-        }
-        
-        return res.status(200).send(event.id);
-
-    } catch (error) {
-        console.log('error')
-        res.status(400).send('failed')
-        
-    }
-
+  res.send(charge);
 });
 
-app.get('/success-payment', (req, res) => {
-    res.send('Payment successfull')
-});
-app.get('/cancel-payment', (req, res) => {
-    res.send('Cancel successfull')
-})
+app.post("/payment-handler", (req, res) => {
+  const rawBody = req.rawBody;
+  const signature = req.headers["x-cc-webhook-signature"];
+  const webhookSecret = COINBASE_WEBHOOK_SECRET;
 
-app.use(express.static(path.join(__dirname, "public")))
+  let event;
+
+  try {
+    event = Webhook.verifyEventBody(rawBody, signature, webhookSecret);
+    // console.log(event);
+
+    if (event.type === "charge:pending") {
+      // received order
+      // user paid, but transaction not confirm on blockchain yet
+      console.log("pending payment");
+    }
+
+    if (event.type === "charge:confirmed") {
+      // fulfill order
+      // charge confirmed
+      console.log("charge confirme");
+    }
+
+    if (event.type === "charge:failed") {
+      // cancel order
+      // charge failed or expired
+      console.log("charge failed");
+    }
+
+    res.send(`success ${event.id}`);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("failure");
+  }
+});
+
+app.get("/success-payment", (req, res) => {
+  res.send("success payment");
+});
+
+app.get("/cancel-payment", (req, res) => {
+  res.send("cancel payment");
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
-
-
-console.log("Puerto", PORT);
+console.log("Server on port", PORT);
